82017994c409081c155ac1ac83e85547835f0b90
neufenda <adriano.neufend@exchange.wwu.de>
2019-07-30 10:50:08 +0200

    Weiteres Errorhandling hinzugefügt. Prints sind nur temporär zum testen da, damit ich eine Ausgabe im Terminal bzw. in der Konsole bekomme.

--- service_test.py
+++ service_test.py
@@ -8,16 +8,20 @@ easydb_info = ''
 def pre_update_function(easydb_context, easydb_info):
     data = easydb_context['data']
     url = "http://localhost:5000/generate"
-    jsonwebtoken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1NjQ0MDgxMzAsIm5iZiI6MTU2NDQwODEzMCwianRpIjoiZTIwYWI4NGEtMzNhZS00M2ZlLTk1ZjEtYjc1ZWIwZTRjODcyIiwiZXhwIjoxNTY0NDA5MDMwLCJpZGVudGl0eSI6ImVhc3lkYjUiLCJmcmVzaCI6dHJ1ZSwidHlwZSI6ImFjY2VzcyJ9.emWz4pHPlV8or7tWWrQEFkMT3Y6zbHYjWfoTKGuTIxY"
-    result = requests.get(url = url, headers={'Authorization': 'Bearer '+jsonwebtoken})
-    json_data = result.json()
-    if "id" in json_data:
-        data['invnr'] = json_data["id"]
-        print (json_data)
-        print (data)
-        return data
-    else:
-        print (json_data)
+    jsonwebtoken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1NjQ0NzU3ODMsIm5iZiI6MTU2NDQ3NTc4MywianRpIjoiMmE4OWQ1ODUtNWEwOC00YzJhLWFkNTgtMDVkODA0YTkzNGMxIiwiZXhwIjoxNTY0NDc2NjgzLCJpZGVudGl0eSI6ImVhc3lkYjUiLCJmcmVzaCI6dHJ1ZSwidHlwZSI6ImFjY2VzcyJ9.Kj5nDuenCU5VKDrnlEOVzFe0E55jPJdHQbb9bPE5dmI"
+    try:
+        result = requests.get(url = url, headers={'Authorization': 'Bearer '+jsonwebtoken})
+        json_data = result.json()
+        if "id" in json_data:
+            data['invnr'] = json_data["id"]
+            print (json_data)
+            print (data)
+            return data
+        else:
+            print (json_data)
+    except requests.exceptions.ConnectionError:
+        print("Could not establish a connection to the service.")
+        return "Could not establish a connection to the service."
 
 
 pre_update_function(easydb_context=easydb_context, easydb_info=easydb_info)
