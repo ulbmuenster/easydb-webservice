3d67bcc98c928233365e51b7ca670f70da63736b
neufenda <adriano.neufend@exchange.wwu.de>
2019-08-20 12:53:46 +0200

    Es gibt nun eine Instiutionstabelle. Das Format der Inventarnummer ist nun an die Institution gebunden und nicht mehr global. Nummern werden nun im definierten Zahlenbereich generiert.

--- run.py
+++ run.py
@@ -5,36 +5,36 @@ from flask_jwt_extended import JWTManager
 
 
 def create_app():
-    app = Flask(__name__, instance_relative_config=True)
-    # Load the configuration from the config folder
-    app.config.from_object("cfg.default")
-    # Load the configuration from the instance folder
-    app.config.from_pyfile('config.py')
-    # Load the file specified by the APP_CONFIG_FILE environment variable
-    # Variables defined here will override those in the default configuration
-    # app.config.from_envvar('APP_CONFIG_FILE')
-    app.config.from_object("cfg.development")
-    # SQLAlchemy
-    from src.InventarnummerModel import db
-    db.init_app(app)
-    with app.app_context():
-        db.create_all()
-    # Flask-Restful API
-    api = Api(app)
-    # JWT configuration
-    jwt = JWTManager(app)
-    # Changes default behaviour and json responses of JWT-Extended.
-    # Source: https://flask-jwt-extended.readthedocs.io/en/latest/changing_default_behavior.html
-    @jwt.invalid_token_loader
-    @jwt.expired_token_loader
-    def token_invalid(token):
-        token_type = token['type']
-        return jsonify({
-            'error': 'The {} token is invalid or expired.'.format(token_type)
-        }), 401
-    # Route configuration
-    # Tells flask_restful which script/class belongs to which request/URL
-    api.add_resource(TokenGenerator.TokenGenerator, "/TokenGenerator")
-    api.add_resource(IDGenerator.IDGenerator, "/generate")
+	app = Flask(__name__, instance_relative_config=True)
+	# Load the configuration from the config folder
+	app.config.from_object("cfg.default")
+	# Load the configuration from the instance folder
+	app.config.from_pyfile('config.py')
+	# Load the file specified by the APP_CONFIG_FILE environment variable
+	# Variables defined here will override those in the default configuration
+	# app.config.from_envvar('APP_CONFIG_FILE')
+	app.config.from_object("cfg.development")
+	# SQLAlchemy
+	from src.Models import db
+	db.init_app(app)
+	with app.app_context():
+		db.create_all()
+	# Flask-Restful API
+	api = Api(app)
+	# JWT configuration
+	jwt = JWTManager(app)
+	# Changes default behaviour and json responses of JWT-Extended.
+	# Source: https://flask-jwt-extended.readthedocs.io/en/latest/changing_default_behavior.html
+	@jwt.invalid_token_loader
+	@jwt.expired_token_loader
+	def token_invalid(token):
+		token_type = token['type']
+		return jsonify({
+			'error': 'The {} token is invalid or expired.'.format(token_type)
+		}), 401
+	# Route configuration
+	# Tells flask_restful which script/class belongs to which request/URL
+	api.add_resource(TokenGenerator.TokenGenerator, "/tokengenerator")
+	api.add_resource(IDGenerator.IDGenerator, "/generate")
 
-    return app
+	return app
